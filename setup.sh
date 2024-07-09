f0fd5f1fcc6b32b1a047865273d78a23ddf5af71
Bernhard Jung <be.ju.le.97@gmail.com>
2024-07-10 01:50:17 +0200

    fix setup

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ mkdir -p "$ZDOTDIR"
 ln -sf "$DOTFILES/zsh/zshenv" "$HOME/.zshenv"
 ln -sf "$DOTFILES/zsh/zshrc" "$ZDOTDIR/.zshrc"
 
-ln -sf $DOTFILES/.emacs.d/            $HOME/
+ln -sf $DOTFILES/emacs.d/            $HOME/.emacs.d
 
